50b03ff1c6c7300c25a28f08ccfb085b6c6b80df
Lidiya Yurkova <ledili2397@mail.ru>
2019-09-10 21:15:48 +0300

    Добавьте файлы проекта.

---
+++ HomeWork/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWork.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string date)
+        {
+            
+            DateTime validatedDate;
+            int quarter;
+            try
+            {
+                validatedDate = Convert.ToDateTime(date);
+                quarter = (validatedDate.Month + 2) / 3;
+                ViewData["quarter"] = "Определённо " + quarter + " квартал.";
+                @ViewData["date"] = date;
+                return View();
+            }
+
+            catch
+            {
+                ViewData["quarter"] = "Введённая строка не является датой!";
+                @ViewData["date"] = date;
+                return View();
+            }
+
+
+
+          
+        }
+
+    }
+}
